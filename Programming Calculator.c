fe7f06a37618bcb425f9c63fba4d6e93bb3b2782
fathss <alwifathu@gmail.com>
2025-01-16 15:07:49 +0700

    Update Programming Calculator.c
    v1.0.0

--- Programming Calculator.c
+++ Programming Calculator.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <math.h>
 
 // Function Prototypes
 void display();
 void arithmetic();
+void conversion();
 void mathematics();
 void programming();
 double add(double a, double b);
@@ -16,16 +18,25 @@ double binary(double (*operation)(double, double));
 double octal(double (*operation)(double, double));
 double hexadecimal(double (*operation)(double, double));
 double differentTypes(double (*operation)(double, double));
-void conversion();
+double baseCheck(char *x);
+int validBase(char *x, int base);
 double baseToDecimal(char *x, int base);
 void decimalToBase(double x, int base);
 
 int main (){
+    char input[64];
     int choice;
-    do
-    {
+    while(1){
         display();
-        scanf("%d", &choice);
+        scanf("%s", input);
+
+        if (!validBase(input, 10)){
+            printf("Invalid input: %s is not a valid number, try again\n\n", input);
+            continue;
+        }
+        else{
+            choice = atoi(input);
+        }
 
         switch (choice)
         {
@@ -39,10 +50,10 @@ int main (){
                 puts("Exit");
                 return 0;
             default:
-                puts("Invalid input, try again\n");
+                puts("Invalid input: input out of range, try again\n");
                 break;
         }
-    }while (choice != 3);
+    }
 
     return 0;
 }
@@ -57,88 +68,107 @@ void display(){
 }
 
 void arithmetic(){
-    int choice, run = 1;
-    do
-    {
-        puts("\nA R I T H M E T I C");
-        puts("-------------------");
-        puts("1. Mathematics");
-        puts("2. Programming");
+    char input[64];
+    int choice;
+
+    puts("\nA R I T H M E T I C");
+    puts("-------------------");
+    puts("1. Mathematics");
+    puts("2. Programming");
+
+    while(1){
         printf("Choose arithmetic operation type: ");
-        scanf("%d", &choice);
+        scanf("%s", input);
 
-
-        switch (choice)
-        {
-            case 1:
-                mathematics();
-                run = 0;
-                break;
-            case 2:
-                programming();
-                run = 0;
-                break;
-            default:
-                puts("Invalid input, try again\n");
-                break;
+        if (!validBase(input, 10)){
+            printf("Invalid input: %s is not a valid number, try again\n\n", input);
+            continue;
         }
-    } while (run);
+        else{
+            choice = atoi(input);
+        }
+
+        if (choice == 1){
+            mathematics();
+            break;
+        }
+        else if (choice == 2){
+            programming();
+            break;
+        }
+        else{
+            printf("Invalid input: input out of range, try again\n\n");
+        }
+    }
 }
 
 void mathematics(){
-    int run = 1;
-    double num1, num2, result;
+    double result, num1, num2;
+    char input1[64], input2[64];
     char op;
-    do
-    {
+    while(1){
         printf("Enter numbers and operation (num1) (operator) (num2): ");
-        scanf("%lf %c %lf", &num1, &op, &num2);
+        scanf("%s %c %s", &input1, &op, &input2);
+
+        if (validBase(input1, 10) && validBase(input2, 10)){
+            num1 = strtod(input1, NULL);
+            num2 = strtod(input2, NULL);
+        }
+        else{
+            printf("Invalid input: not a valid number, try again\n\n");
+            continue;
+        }
 
         switch (op)
         {
             case '+':
-                result = num1 + num2;
-                run = 0;
+                result = add(num1, num2);
                 break;
             case '-':
-                result = num1 - num2;
-                run = 0;
+                result = subtract(num1, num2);
                 break;
             case '*':
-                result = num1 * num2;
-                run = 0;
+                result = multiply(num1, num2);
                 break;
             case '/':
                 if (num2 == 0){
-                    printf("Division by 0!\nProgram will stop\n");
-                    exit(0);
+                    printf("Invalid input: division by zero, try again\n\n");
+                    continue;
                 }
-                result = num1 / num2;
-                run = 0;
+                result = divide(num1, num2);
                 break;
             default:
-                puts("Invalid operator, try again");
-                break;
+                printf("Invalid input: %c is not a valid operator, try again\n\n", op);
+                continue;
         }
-    } while (run);
 
-    printf("Result: %g\n\n", result);
+        printf("Result: %g\n\n", result);
+        break;
+    }
 }
 
 void programming(){
-    int choice, run = 1;
     double result;
-    do
-    {
+
+    while(1){
+        char input[64];
+        int choice;
         puts("1. Binary");
         puts("2. Octal");
         puts("3. Hexadecimal");
         puts("4. Different types");
-        printf("Choose data type for arithmetic operation: ");
-        scanf("%d", &choice);
+        printf("Choose numeral type for arithmetic operation: ");
+        scanf("%s", input);
+
+        if (!validBase(input, 10)){
+            printf("Invalid input: %s is not a valid number, try again\n\n", input);
+        }
+        else{
+            choice = atoi(input);
+        }
 
         if (choice < 1 || choice > 4){
-            printf("Invalid input, try again\n\n");
+            printf("Invalid input: input out of range, try again\n\n");
             continue;
         }
 
@@ -148,7 +178,20 @@ void programming(){
         puts("3. Multiplication");
         puts("4. Division");
         printf("Choose desired operation: ");
-        scanf("%d", &operation);
+        scanf("%s", input);
+
+        if (!validBase(input, 10)){
+            printf("Invalid input: %s is not a valid number, try again\n\n", input);
+            continue;
+        }
+        else{
+            operation = atoi(input);
+        }
+
+        if (operation < 1 || operation > 4){
+            printf("Invalid input: input out of range, try again\n\n");
+            continue;
+        }
 
         switch (choice)
         {
@@ -168,7 +211,6 @@ void programming(){
                         result = binary(divide);
                         break;
                 }
-                run = 0;
                 break;
             case 2:
                 switch (operation)
@@ -186,7 +228,6 @@ void programming(){
                         result = octal(divide);
                         break;
                 }
-                run = 0;
                 break;
 
             case 3:
@@ -205,7 +246,6 @@ void programming(){
                         result = hexadecimal(divide);
                         break;
                 }
-                run = 0;
                 break;
 
             case 4:
@@ -224,22 +264,120 @@ void programming(){
                         result = differentTypes(divide);
                         break;
                 }
-                run = 0;
-                break;
-
-            default:
-                puts("Invalid input, try again");
                 break;
         }
-    } while (run);
-    printf("\nBinary       : ");
-    decimalToBase(result, 2);
-    printf("\nDecimal      : %g", result);
-    printf("\nOctal        : ");
-    decimalToBase(result, 8);
-    printf("\nHexadecimal  : ");
-    decimalToBase(result, 16);
-    puts("\n");
+        if (result < 0) continue;
+        printf("\nBinary       : ");
+        decimalToBase(result, 2);
+        printf("\nDecimal      : %g", result);
+        printf("\nOctal        : ");
+        decimalToBase(result, 8);
+        printf("\nHexadecimal  : ");
+        decimalToBase(result, 16);
+        puts("\n");
+        break;
+    }
+}
+
+void conversion(){
+
+    double dec;
+    while(1){
+        char input[64];
+        int choice;
+        char decimal[32], binary[32], octal[32], hexadecimal[32];
+        puts("\nBASE CONVERSION");
+        puts("----------------");
+        puts("1. Decimal");
+        puts("2. Binary");
+        puts("3. Octal");
+        puts("4. Hexadecimal");
+        printf("Choose starting type: ");
+        scanf("%s", input);
+
+        if (!validBase(input, 10)){
+            printf("Invalid input: %s is not a valid number, try again\n\n", input);
+            continue;
+        }
+        else{
+            choice = atoi(input);
+        }
+
+        if(choice < 1 || choice > 4){
+            printf("Invalid input: input out of range, try again\n\n");
+            continue;
+        }
+        else if (choice == 1){
+            printf("Enter decimal : ");
+            scanf("%s", decimal);
+
+            if (!validBase(decimal, 10)){
+                printf("Invalid input: not a valid decimal, try again\n\n");
+                continue;
+            }
+
+            dec = strtod(decimal, NULL);
+            printf("Binary        : ");
+            decimalToBase(dec, 2);
+            printf("\nOctal         : ");
+            decimalToBase(dec, 8);
+            printf("\nHexadecimal   : ");
+            decimalToBase(dec, 16);
+            puts("\n");
+        }
+        else if (choice == 2){
+            printf("Enter binary  : ");
+            scanf("%s", binary);
+
+            if (!validBase(binary, 2)){
+                printf("Invalid input: not a valid binary, try again\n\n");
+                continue;
+            }
+
+            dec = baseToDecimal(binary, 2);
+            printf("Decimal       : %g", dec);
+            printf("\nOctal         : ");
+            decimalToBase(dec, 8);
+            printf("\nHexadecimal   : ");
+            decimalToBase(dec, 16);
+            puts("\n");
+        }
+        else if (choice == 3){
+            printf("Enter octal : ");
+            scanf("%s", octal);
+
+            if (!validBase(octal, 8)){
+                printf("Invalid input: not a valid octal, try again\n\n");
+                continue;
+            }
+
+            dec = baseToDecimal(octal, 8);
+            printf("Decimal       : %g", dec);
+            printf("\nBinary        : ");
+            decimalToBase(dec, 2);
+            printf("\nHexadecimal   : ");
+            decimalToBase(dec, 16);
+            puts("\n");
+        }
+        else if (choice == 4){
+            printf("Enter Hexadecimal : ");
+            scanf("%s", hexadecimal);
+
+            if (!validBase(hexadecimal, 16)){
+                printf("Invalid input: not a valid hexadecimal, try again\n\n");
+                continue;
+            }
+
+            dec = baseToDecimal(hexadecimal, 16);
+            printf("Decimal       : %g", dec);
+            printf("\nBinary        : ");
+            decimalToBase(dec, 2);
+            printf("\nOctal         : ");
+            decimalToBase(dec, 8);
+            puts("\n");
+        }
+        break;
+    }
 }
 
 double add(double a, double b){
@@ -255,15 +393,31 @@ double multiply(double a, double b){
 }
 
 double divide(double a, double b){
+    if (b == 0){
+        printf("Invalid input: division by zero, try again\n\n");
+        return -1;
+    }
     return a / b;
 }
 
 double binary(double (*operation)(double, double)){
     char binary1[32], binary2[32];
+
     printf("Enter binary 1: ");
     scanf("%s", binary1);
+
+    if(!validBase(binary1, 2)){
+        printf("Invalid input: not a valid binary, try again\n\n");
+        return -1;
+    }
+
     printf("Enter binary 2: ");
     scanf("%s", binary2);
+
+    if(!validBase(binary2  , 2)){
+        printf("Invalid input: not a valid binary, try again\n\n");
+        return -1;
+    }
 
     double dec1 = baseToDecimal(binary1, 2);
     double dec2 = baseToDecimal(binary2, 2);
@@ -273,10 +427,22 @@ double binary(double (*operation)(double, double)){
 
 double octal(double (*operation)(double, double)){
     char octal1[32], octal2[32];
+
     printf("Enter octal 1: ");
     scanf("%s", octal1);
+
+    if(!validBase(octal1, 8)){
+        printf("Invalid input: not a valid octal, try again\n\n");
+        return -1;
+    }
+
     printf("Enter octal 2: ");
     scanf("%s", octal2);
+
+    if(!validBase(octal2, 8)){
+        printf("Invalid input: not a valid octal, try again\n\n");
+        return -1;
+    }
 
     double dec1 = baseToDecimal(octal1, 8);
     double dec2 = baseToDecimal(octal2, 8);
@@ -286,10 +452,22 @@ double octal(double (*operation)(double, double)){
 
 double hexadecimal(double (*operation)(double, double)){
     char hex1[32], hex2[32];
-    printf("Enter octal 1: ");
+
+    printf("Enter hex 1: ");
     scanf("%s", hex1);
-    printf("Enter octal 2: ");
+
+    if(!validBase(hex1, 16)){
+        printf("Invalid input: not a valid hexadecimal, try again\n\n");
+        return -1;
+    }
+
+    printf("Enter hex 2: ");
     scanf("%s", hex2);
+
+    if(!validBase(hex2, 16)){
+        printf("Invalid input: not a valid hexadecimal, try again\n\n");
+        return -1;
+    }
 
     double dec1 = baseToDecimal(hex1, 16);
     double dec2 = baseToDecimal(hex2, 16);
@@ -308,108 +486,100 @@ double differentTypes(double (*operation)(double, double)) {
 
     printf("Enter input 1: ");
     scanf("%s", input);
-    char *ptr = input;
 
-    if (strncmp(input, "0b", 2) == 0 || strncmp(input, "0B", 2) == 0){
-        ptr += 2;
-        dec1 = baseToDecimal(ptr, 2); // Binary
-    } else if (strncmp(input, "0x", 2) == 0 || strncmp(input, "0X", 2) == 0){
-        ptr += 2;
-        dec1 = baseToDecimal(ptr, 16); // Hexadecimal
-    } else if (input[0] == '0'){
-        ptr++;
-        dec1 = baseToDecimal(ptr, 8); // Octal
-    } else{
-        dec1 = strtod(ptr, NULL); // Decimal
+    dec1 = baseCheck(input);
+    if (dec1 < 0){
+        printf("Invalid input: not a valid prefix, try again\n\n");
+        return -1;
     }
 
     printf("Enter input 2: ");
     scanf("%s", input);
-    ptr = input;
 
-    if (strncmp(input, "0b", 2) == 0 || strncmp(input, "0B", 2) == 0){
-        ptr += 2;
-        dec2 = baseToDecimal(ptr, 2); // Binary
-    } else if (strncmp(input, "0x", 2) == 0 || strncmp(input, "0X", 2) == 0){
-        ptr += 2;
-        dec2 = baseToDecimal(ptr, 16); // Hexadecimal
-    } else if (input[0] == '0'){
-        ptr++;
-        dec2 = baseToDecimal(ptr, 8); // Octal
-    } else{
-        dec2 = strtod(ptr, NULL); // Decimal
+    dec2 = baseCheck(input);
+    if (dec2 < 0){
+        printf("Invalid input: not a valid prefix, try again\n\n");
+        return -1;
     }
 
     return operation(dec1, dec2);
 }
 
-void conversion(){
-    int choice;
-    char decimal[32], binary[32], octal[32], hexadecimal[32];
-    double dec;
-    puts("\nBASE CONVERSION");
-    puts("----------------");
-    puts("1. Decimal");
-    puts("2. Binary");
-    puts("3. Octal");
-    puts("4. Hexadecimal");
-    printf("Choose starting type: ");
-    scanf("%d", &choice);
-
-    switch (choice)
-    {
-        case 1:
-            printf("Enter decimal : ");
-            scanf("%s", decimal);
-            dec = strtod(decimal, NULL);
-            printf("Binary        : ");
-            decimalToBase(dec, 2);
-            printf("\nOctal         : ");
-            decimalToBase(dec, 8);
-            printf("\nHexadecimal   : ");
-            decimalToBase(dec, 16);
-            puts("\n");
-            break;
-        case 2:
-            printf("Enter binary : ");
-            scanf("%s", binary);
-            dec = baseToDecimal(binary, 2);
-            printf("Decimal       : %g", dec);
-            printf("\nOctal         : ");
-            decimalToBase(dec, 8);
-            printf("\nHexadecimal   : ");
-            decimalToBase(dec, 16);
-            puts("\n");
-            break;
-        case 3:
-            printf("Enter octal : ");
-            scanf("%s", octal);
-            dec = baseToDecimal(octal, 8);
-            printf("Decimal       : %g", dec);
-            printf("\nBinary        : ");
-            decimalToBase(dec, 2);
-            printf("\nHexadecimal   : ");
-            decimalToBase(dec, 16);
-            puts("\n");
-            break;
-        case 4:
-            printf("Enter Hexadecimal : ");
-            scanf("%s", hexadecimal);
-            dec = baseToDecimal(hexadecimal, 16);
-            printf("Decimal       : %g", dec);
-            printf("\nBinary        : ");
-            decimalToBase(dec, 2);
-            printf("\nOctal         : ");
-            decimalToBase(dec, 8);
-            puts("\n");
-            break;
-        default:
-            puts("Invalid input, try again\n");
-            conversion();
-            break;
+// Checking what base from input
+double baseCheck(char *x){
+    // Binary type chcek
+    if (strncmp(x, "0b", 2) == 0 || strncmp(x, "0B", 2) == 0){
+        x += 2;
+        if (!validBase(x, 2)) return -1;
+        return baseToDecimal(x, 2);
+    // Hexadecimal type chcek
+    }
+    else if (strncmp(x, "0x", 2) == 0 || strncmp(x, "0X", 2) == 0){
+        x += 2;
+        if (!validBase(x, 16)) return -1;
+        return baseToDecimal(x, 16);
+    }
+    // Octal type chcek
+    else if (x[0] == '0'){
+        x++;
+        if (!validBase(x, 8)) return -1;
+        return baseToDecimal(x, 8);
+    }
+    // Decimal type chcek
+    else{
+        if (!validBase(x, 10)) return -1;
+        return strtod(x, NULL);
     }
 }
 
+// Checking if the base input is valid
+int validBase(char *x, int base){
+    switch (base)
+    {
+        case 2:
+            for (char *i = x; *i != '\0'; i++){
+                if (isdigit(*i) && *i >= '0' && *i <= '1'){
+                    continue;
+                } else{
+                    return 0;
+                }
+            }
+            break;
+
+        case 8:
+            for (char *i = x; *i != '\0'; i++){
+                if (isdigit(*i) && *i >= '0' && *i <= '7'){
+                    continue;
+                } else{
+                    return 0;
+                }
+            }
+            break;
+
+        case 10:
+            for (char *i = x; *i != '\0'; i++){
+                if (isdigit(*i) || *i == '.'){
+                    continue;
+                } else{
+                    return 0;
+                }
+            }
+            break;
+
+        case 16:
+            for (char *i = x; *i != '\0'; i++){
+                if (*i >= '0' && *i <= '9' || *i >= 'A' && *i <= 'F'){
+                    continue;
+                } else{
+                    return 0;
+                }
+            }
+            break;
+    }
+    return 1;
+}
+
+// Convert other base to decimal
 double baseToDecimal(char *x, int base){
     double dec = 0.0, fraction = 0.0;
     char *dot = strstr(x, ".");
@@ -418,7 +588,7 @@ double baseToDecimal(char *x, int base){
         dot = x + strlen(x);
     }
 
-    // Before the dot
+    // Integer part
     int index = dot - x - 1;
     int value;
     for (int i = 0; *(x + i) != '.' && *(x + i) != '\0'; i++){
@@ -430,7 +600,7 @@ double baseToDecimal(char *x, int base){
         dec += value * pow(base, index - i);
     }
 
-    // After the dot
+    // Decimal part
     if (*(dot) != '\0') {
         dot++;
         for (int i = 1; *dot != '\0'; i++, dot++){
@@ -447,6 +617,7 @@ double baseToDecimal(char *x, int base){
     return dec;
 }
 
+// Convert decimal to other base
 void decimalToBase(double x, int base){
     char intResult[65], floatResult[65];
     char digits[] = "0123456789ABCDEF";
