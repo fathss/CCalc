6f1d53a4286208bcf2e9cc404e46697d6cf299ec
fathss <alwifathu@gmail.com>
2025-05-29 11:52:19 +0700

    Getting rid of error_type.h as it is not needed

--- include/error_handling.h
+++ include/error_handling.h
@@ -1,7 +1,18 @@
 #ifndef ERROR_MSG_H
 #define ERROR_MSG_H
 
-#include "error_type.h"
+typedef enum
+{
+    INVALID_PREFIX,
+    INVALID_NUMBER,
+    INVALID_OPERATOR,
+    INPUT_OUT_OF_RANGE,
+    DIVISION_BY_ZERO,
+    INVALID_BINARY,
+    INVALID_OCTAL,
+    INVALID_HEX,
+    INVALID_DECIMAL
+} ErrorType;
 
 void error(ErrorType errorType);
 
