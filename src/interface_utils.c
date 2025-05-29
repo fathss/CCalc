2c86abbaae3d929251962639f651f41a2c0eea76
fathss <alwifathu@gmail.com>
2025-05-29 21:15:38 +0700

    Small debugging

--- src/interface_utils.c
+++ src/interface_utils.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "interface_utils.h"
+#include "base_utils.h"
 #include "error_handling.h"
 
 int get_validated_choice(const char *prompt, int min, int max)
